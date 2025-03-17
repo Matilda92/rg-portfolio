8a645456c269ece1e4b6b0427e80ca5b26532c23
Matilda92 <151093009+Matilda92@users.noreply.github.com>
2025-03-17 08:21:52 +0000

    march17

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,29 @@
-const dotenv = require("dotenv");
-
-if (process.env.ENVIRONMENT !== "production") {
-  dotenv.config();
-}
-
-const { spaceId, accessToken } = process.env;
+require("dotenv").config({ 
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
 module.exports = {
   siteMetadata: {
     title: `Rohit Gupta`,
     description: `Personal Site`,
-    author: `@rohitguptab`
+    author: `@rohitguptab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`, 
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId,
-        accessToken
-      }
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -39,9 +36,9 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#333`,
-        icon: `src/images/fev_icon.png` // This path is relative to the root of the site.
-      }
-    }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
-    `gatsby-plugin-offline`
-  ]
+        icon: `src/images/fev_icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 };
