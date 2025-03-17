44bea44dcb099eeee634581a658c56675e1d8983
Matilda92 <151093009+Matilda92@users.noreply.github.com>
2025-03-18 01:01:28 +0300

    Update blogs.js

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -10,47 +10,35 @@ import SEO from "../components/seo";
 export default class Blogs extends Component {
   render() {
     const { data } = this.props;
+    const blogPosts = data?.allContentfulBlogPost?.edges || [];
+
     return (
       <Layout>
-        <SEO
-          title="Blogs"
-          keywords={[`Rohit Gupta`, `Frontend Developer`, `Developer`, `Blogs`]}
-        />
+        <SEO title="Blogs" keywords={["Rohit Gupta", "Frontend Developer", "Developer", "Blogs"]} />
         <div className="site-container blogs-page" id="Blogs">
           <div className="container">
             <div className="section-head">
               <h1 className="line-heading h2">Blogs</h1>
             </div>
-            <ul
-              className={`blogs-list ${
-                data.allContentfulBlogs.edges.length < 5 ? "few-blogs" : ""
-              }`}
-            >
-              {data.allContentfulBlogs.edges.map((item, index) => {
-                return (
-                  <li key={index} className="item">
-                    <div className="inner">
-                      <Link className="link" to={`/${item.node.slug}`} />
-                      {item.node.featureImage ? (
-                        <Img
-                          fluid={item.node.featureImage.fluid}
-                          objectFit="cover"
-                          objectPosition="50% 50%"
-                        />
-                      ) : (
-                        <div className="no-image"></div>
-                      )}
-                      <div className="details">
-                        <h3 className="title">{item.node.title}</h3>
-                        <span className="date">
-                          <i className="fas fa-calendar-alt"></i>{" "}
-                          {moment(item.node.createdAt).format("LL")}
-                        </span>
-                      </div>
+            <ul className={`blogs-list ${blogPosts.length < 5 ? "few-blogs" : ""}`}>
+              {blogPosts.map(({ node }, index) => (
+                <li key={index} className="item">
+                  <div className="inner">
+                    <Link className="link" to={`/${node.slug}`} />
+                    {node.featureImage ? (
+                      <Img fluid={node.featureImage.fluid} alt={node.title} objectFit="cover" objectPosition="50% 50%" />
+                    ) : (
+                      <div className="no-image"></div>
+                    )}
+                    <div className="details">
+                      <h3 className="title">{node.title}</h3>
+                      <span className="date">
+                        <i className="fas fa-calendar-alt"></i> {moment(node.createdAt).format("LL")}
+                      </span>
                     </div>
-                  </li>
-                );
-              })}
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -61,20 +49,14 @@ export default class Blogs extends Component {
 
 export const pageQuery = graphql`
   query BlogsQuery {
-    allContentfulBlogs(sort: {fields: createdAt, order: DESC}) {
+    allContentfulBlogPost(sort: { fields: createdAt, order: DESC }) {
       edges {
         node {
           title
           slug
           featureImage {
             fluid(maxWidth: 1500) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
+              ...GatsbyContentfulFluid
             }
           }
           createdAt
